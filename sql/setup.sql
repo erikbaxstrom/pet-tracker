aa21654df65adc14a35d81d4019cd3baa3f69bc9
Tyler Watson <tylerww91@gmail.com>
2023-02-20 12:38:06 -0800

    builds pets, users_pets tables

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,7 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS pets CASCADE;
 
 CREATE TABLE users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -9,3 +10,20 @@ CREATE TABLE users (
   first_name VARCHAR NOT NULL,
   last_name VARCHAR NOT NULL
 );
+
+CREATE TABLE pets (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  breed VARCHAR NOT NULL,
+  emergency_contact VARCHAR,
+  vet VARCHAR,
+  notes VARCHAR
+);
+
+CREATE TABLE users_pets (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  pet_id BIGINT,
+  user_id BIGINT,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (pet_id) REFERENCES pets(id)
+)

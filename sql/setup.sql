a0aad115698638e247520aec4f3c8a76ea20e18c
Tyler Watson <tylerww91@gmail.com>
2023-02-20 14:42:47 -0800

    removes first/lastname

--- sql/setup.sql
+++ sql/setup.sql
@@ -2,13 +2,12 @@
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS pets CASCADE;
+DROP TABLE IF EXISTS users_pets CASCADE;
 
 CREATE TABLE users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   email VARCHAR,
-  password_hash VARCHAR NOT NULL,
-  first_name VARCHAR NOT NULL,
-  last_name VARCHAR NOT NULL
+  password_hash VARCHAR NOT NULL
 );
 
 CREATE TABLE pets (
